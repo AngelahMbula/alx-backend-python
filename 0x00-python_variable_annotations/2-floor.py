33352bc556004af56ecfec692aef213d233da280
AngelahMbula <mbula.angelah@gmail.com>
2023-03-31 09:26:18 +0000

    initial commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function floor which takes float n as argument."""
+import math
+
+
+def floor(n: float) -> int:
+    """returns floor of the float."""
+    return math.floor(n)
